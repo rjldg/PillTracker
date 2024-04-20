8dbb1a1052bf2aa5747d6640b3d4af1204257b69
rjldg <rjlimdeguzman@gmail.com>
2024-04-20 22:34:23 +0800

    Added login bg image via container control

--- views/login.py
+++ views/login.py
@@ -5,9 +5,16 @@ import flet as ft
 class LoginView(FletView):
     def __init__(self, controller, model):
         view = [
-            ft.Text(ref=model.example_title, size=30),
-            ft.ElevatedButton(
-                "Go to register view", on_click=controller.navigate_secundary
-            ),
+            ft.Container(
+                image_src='../images/login_bg.png',
+                image_fit=ft.ImageFit.FILL,
+                expand=True,
+                content=ft.Row(controls=[
+                    ft.Text(ref=model.example_title, size=30),
+                    ft.ElevatedButton(
+                        "Go to register view", on_click=controller.navigate_secundary
+                    )
+                ])
+            )
         ]
         super().__init__(model, view, controller)
