d7a4897477719059629b29e4f95cd1cff012d473
azra-dev <azraelanonas@gmail.com>
2024-05-01 20:23:49 +0800

    Implemented User Profile

--- PillTracker.py
+++ PillTracker.py
@@ -26,33 +26,34 @@ from mvc.controllers.account import AccountController
 def main(page: ft.Page):
     ### MVC set-up ###
     routes_handler = RouteHandler(page)
-    model_list = [LoginModel(), RegisterModel(), HomeModel(), ScheduleModel(), LoginModel()]
+    model_list = [LoginModel(), RegisterModel(), HomeModel(), ScheduleModel(), AccountModel()]
     controller_list = [
-        LoginController(page, model_list[0]), 
+           LoginController(page, model_list[0]), 
         RegisterController(page, model_list[1]), 
-        HomeController(page, model_list[2]), 
+            HomeController(page, model_list[2]), 
         ScheduleController(page, model_list[3]), 
-        AccountController(page, model_list[4])
+         AccountController(page, model_list[4]),
     ]
     view_list = [
-        LoginView(controller_list[0], model_list[0]),
+           LoginView(controller_list[0], model_list[0]),
         RegisterView(controller_list[1], model_list[1]),
-        HomeView(controller_list[2], model_list[2]),
+            HomeView(controller_list[2], model_list[2]),
         ScheduleView(controller_list[3], model_list[3]),
-        AccountView(controller_list[4], model_list[4]),
+         AccountView(controller_list[4], model_list[4]),
     ]
     pages = [
         [""         , model_list[0], controller_list[0], view_list[0]],
         ["register" , model_list[1], controller_list[1], view_list[1]],
         ["home"     , model_list[2], controller_list[2], view_list[2]],
         ["schedule" , model_list[3], controller_list[3], view_list[3]],
-        ["account"  , model_list[4], controller_list[4], view_list[4]]
+        ["account"  , model_list[4], controller_list[4], view_list[4]],
     ]
     for p in pages:
         p[1].controller = p[2]
         routes_handler.register_route(f"/{p[0]}", p[3].content)
 
-    # Global settings for pages
+
+    ### Global settings for pages
     theme = ft.Theme()
     platforms = ["android", "ios", "macos", "linux", "windows"]
     for platform in platforms:  # Removing animation on route change.
--- mvc/models/account.py
+++ mvc/models/account.py
@@ -4,5 +4,33 @@ import flet as ft
 
 class AccountModel(FletModel):
     @data
-    def dummy(self):
+    def example_title(self):
+        return "This is the account view!"
+    
+    @data
+    def firstname(self):
+        return ""
+    
+    @data
+    def lastname(self):
+        return ""
+    
+    @data
+    def username(self):
+        return ""
+    
+    @data
+    def email(self):
+        return ""
+    
+    @data
+    def gender(self):
+        return ""
+    
+    @data
+    def password(self):
+        return ""
+    
+    @data
+    def confirm_password(self):
         return ""
